6374aa641e07cb8ce54fc5d2931295181e891252
zzacong <ongzz100@gmail.com>
2020-11-21 21:48:13 +1100

    add link

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+/**
+ * Traversy Media
+ * Build a Wildfire Tracker With React & NASA API
+ * YouTube: https://youtu.be/ontX4zfVqK8
+ */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
